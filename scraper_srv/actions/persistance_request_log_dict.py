8c9266889190fa7af84ac1adb88f166f5d953797
Molina Milla Antonio Ramon <arm@elcaNet.local>
2021-07-18 12:37:46 +0200

    migrate to oop first step

--- scraper_srv/actions/persistance_request_log_dict.py
+++ scraper_srv/actions/persistance_request_log_dict.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 from repository.mongodb_repository import MongoDBRepository
-from scrapers import web_scraper
+from scrapers.web_scraper import web_scrapper
 from models.request_log import *
  
 def persist_request_log(url):
     if(url):
         start_date = datetime.now().isoformat()
-        payload = web_scraper.get_href(url)
+        payload = web_scrapper.get_href(url)
         end_date = datetime.now().isoformat()
         
         request_log_dict = request_log(start_date, end_date, url, payload)
--- scraper_srv/app.py
+++ scraper_srv/app.py
@@ -1,18 +1,9 @@
 import os
-import json
 
 from actions import persistance_request_log_dict
-from api import runner
+from entrypoints import fast_api, auto_scraper
 
 if "URL_TO_SCRAP" in os.environ:
-    print("================================================")
-    print("Docker URL_TO_SCRAP received, getting hrefs...")
-    request_log = persistance_request_log_dict.persist_request_log(os.environ["URL_TO_SCRAP"])
-    print(request_log["uuid"])
-    print(json.dumps(request_log["payload"], indent=2))
-    print("Scraping complete")
-    print("================================================")
-
+    auto_scraper.AutoScrap.auto_web_scrap(os.environ["URL_TO_SCRAP"])
 else:
-    print("initialize API")
-    runner.startApiService()
+    fast_api.startApiService()
