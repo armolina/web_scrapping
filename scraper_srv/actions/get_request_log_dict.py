8b4e3b79d98718afc5a334bebd101545f3181629
armolina <armolinamilla@gmail.com>
2021-07-19 08:49:16 +0200

    modify some folders to clean the project

--- scraper_srv/actions/get_request_log_dict.py
+++
@@ -1,8 +0,0 @@
-from repository.mongodb_repository import MongoDBRepository
-
-def get_request_log(request_log_id):
-    if(request_log_id):
-        request_log = MongoDBRepository.getDataById(request_log_id)
-        return request_log
-    else:
-        return False
--- scraper_srv/actions/list_request_log_dict.py
+++
@@ -1,5 +0,0 @@
-from repository.mongodb_repository import MongoDBRepository
-
-def get_request_log_list():
-    list_requests = MongoDBRepository.getData()
-    return list_requests
